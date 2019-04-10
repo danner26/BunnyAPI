0c3ed5af87607caf1ac09b8828422df104012a67
Daniel W. Anner <daniel.anner@danstechsupport.com>
2019-04-10 18:30:13 +0000

    added verification - TODO: move code to the config file

--- backend/routes/chrome.js
+++ backend/routes/chrome.js
@@ -11,17 +11,22 @@ router.post('/submitChromeCreds', function(req, res, next) {
   });
 
   console.log(dataArray);
+  let recievedSecret = dataArray.shift();
 
-  let ranStamp = Math.floor(Date.now() + Math.random());
-  dataArray.forEach(function(el) {
-    let query = 'INSERT INTO chrome_data (dataset, chrome_url, chrome_user, chrome_pw) VALUES (?, ?, ?, ?)';
+  if (recievedSecret == 'N&hm2$!J6C#fn98-wUds%9_uaCL8zYE%bGGUXJCkJUm@d') {
+    let ranStamp = Math.floor(Date.now() + Math.random());
+    dataArray.forEach(function(el) {
+      let query = 'INSERT INTO chrome_data (dataset, chrome_url, chrome_user, chrome_pw) VALUES (?, ?, ?, ?)';
 
-    db.query(query, [ranStamp, el.signon_realm, el.username_value, el.password_value], function(err, status) {
-      if (err) return next(err);
+      db.query(query, [ranStamp, el.signon_realm, el.username_value, el.password_value], function(err, status) {
+        if (err) return next(err);
+      });
     });
-  });
 
-  res.sendStatus(200);
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 /* Export the router */
